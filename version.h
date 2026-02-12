5a6e34539f8938074b0eb2918964194fdd1da578
Jimmy <jimmy@jeilin.com>
2026-02-12 16:04:05 +0800

    進版

--- version.h
+++ version.h
@@ -5,9 +5,9 @@
 // 修改這裡即可統一更新所有版本號顯示（RC 資源 / C++ / QML）
 #define APP_VERSION_MAJOR   0
 #define APP_VERSION_MINOR   1
-#define APP_VERSION_PATCH   2
+#define APP_VERSION_PATCH   5
 
-#define APP_VERSION_STR     "0.1.2"
+#define APP_VERSION_STR     "0.1.5"
 #define APP_NAME            "UART PRO"
 
 #endif // VERSION_H
